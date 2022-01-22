8a5c70fc56cf7c639374457c4f889eb3345b3afa
plum429 <liujunyi_coder@163.com>
2022-01-22 20:02:12 +0800

    dashboard面板

--- src/template/admin/src/views/dashboard/index.vue
+++ src/template/admin/src/views/dashboard/index.vue
@@ -1,23 +1,31 @@
 <template>
-  <div>
-    {{list}}
-    <Dialog v-model="list" :visible.sync="visible"></Dialog>
-    <el-button @click="visible=!visible">测试</el-button>
-  </div>
+  <curd-card>
+    <p align="center">
+      <el-image src="http://file.huamzl.wang/tuchuang/20221211449/u8of24f87i_plumadmin_logo_big.png"></el-image>
+    </p>
+    <p align="center"><code>PlumAdmin</code> 基于Thinkphp6 & VUE2开发的后台管理框架</p>
+    <p align="center">
+      <a target="_blank" style="color:#409eff;" href="http://preview.plumcloud.top/">演示地址</a> | <a style="color:#409eff;" target="_blank"  href="http://doc.plumcloud.top/">文档</a>
+    </p>
+    <p align="center">
+      <img style="margin-left:10px;" src="https://svg.hamm.cn/badge.svg?key=Base&value=ThinkPHP6"/>
+      <img style="margin-left:10px;" src="https://svg.hamm.cn/badge.svg?key=Data&value=MySQL5.6"/>
+      <img style="margin-left:10px;" src="https://svg.hamm.cn/badge.svg?key=Runtime&value=PHP7.4"/>
+      <img style="margin-left:10px;" src="https://svg.hamm.cn/badge.svg?key=License&value=MIT"/>
+    </p>
+    <p>文档还没有完善,有问题可以加微信或者qq群交流</p>
+
+    <div>
+      <el-image style="width: 200px" src="http://file.huamzl.wang/tuchuang/20221221730/nx70zvz3r5_39b0baf031ba19e0c9dd73403b8ac45.jpg" />
+      <el-image style="width: 200px" src="http://file.huamzl.wang/tuchuang/20221221800/n5esxpc7kh_1642845652675.png" />
+
+    </div>
+  </curd-card>
 </template>
 <script>
-import Dialog from '@/components/AttachmentLibrary'
 
 export default {
-  name:'dashboard',
-  components: {Dialog},
-  data() {
-    return {
-      visible: true,
-      list:undefined
-    }
-  },
-  methods: {}
+  name: 'dashboard',
 }
 </script>
 <style scoped lang="scss">
